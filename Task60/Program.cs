86bb2383357192e6b7b502aadf4458478173be54
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-21 20:24:47 +0700

    decide task60

---
+++ Task60/Program.cs
@@ -0,0 +1,53 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+//
+int[,,] Create3dArray(int x, int y, int z)
+{
+    int[,,] array = new int[x, y, z];
+    return array;
+}
+//
+int[,,] Fill3dArray(int[,,] array, int min, int max)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rand.Next(min, max);
+            }
+        }
+    }       
+    return array;
+}
+//
+void Print3dArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++, Console.WriteLine())
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//Console.WriteLine()
+        {
+            for (int k = 0; k < array.GetLength(2); k++)//Console.WriteLine()
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k})\t");
+            }
+        }
+    }
+}
+//
+int[,,] array = Create3dArray(2, 2, 2);
+array = Fill3dArray(array, 11, 99);
+Print3dArray(array);
+
+
+
+
+
