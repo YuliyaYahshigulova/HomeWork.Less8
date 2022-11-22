5c0db9627060f03ff3c7b8604397e24035c67109
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-22 12:32:00 +0700

    change decision of task60

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -31,14 +31,15 @@ int[,,] Fill3dArray(int[,,] array, int min, int max)
 //
 void Print3dArray(int[,,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++, Console.WriteLine())
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)//Console.WriteLine()
-        {
-            for (int k = 0; k < array.GetLength(2); k++)//Console.WriteLine()
+        for (int j = 0; j < array.GetLength(1); j++)
+        { 
+            for (int k = 0; k < array.GetLength(2); k++)
             {
                 Console.Write($"{array[i, j, k]} ({i},{j},{k})\t");
             }
+            Console.WriteLine();
         }
     }
 }
